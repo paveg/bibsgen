b3bd2db0d460a3938cc97d8c6519748a3c46902a
Ryota Ikezawa <pavegy@gmail.com>
2024-09-08 02:59:29 +0900

    feat(size and color): fix

--- src/components/generator.tsx
+++ src/components/generator.tsx
@@ -1,7 +1,10 @@
 import { useEffect, useRef, useState } from 'react';
+import type { Color } from 'react-input-color';
+import InputColor from 'react-input-color';
 
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 import {
   SelectTrigger,
   Select,
@@ -14,6 +17,28 @@ export const Generator = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const [text, setText] = useState('');
   const [font, setFont] = useState('electricboots');
+  const [fontColor, setFontColor] = useState<Color>({
+    a: 0,
+    b: 0,
+    g: 0,
+    h: 0,
+    hex: 'black',
+    r: 0,
+    rgba: '',
+    s: 0,
+    v: 0,
+  });
+  const [backgroundColor, setBackgroundColor] = useState<Color>({
+    a: 0,
+    b: 0,
+    g: 0,
+    h: 0,
+    hex: 'white',
+    r: 0,
+    rgba: '',
+    s: 0,
+    v: 0,
+  });
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -21,20 +46,45 @@ export const Generator = () => {
       const ctx = canvas.getContext('2d');
       if (ctx) {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.font = ['48px', font].join(' ');
+
+        // Background color
+        ctx.fillStyle = backgroundColor.hex;
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        ctx.font = ['100px', font].join(' ');
         ctx.textAlign = 'center';
         ctx.textBaseline = 'middle';
+
+        ctx.fillStyle = fontColor.hex;
         ctx.fillText(text, canvas.width / 2, canvas.height / 2);
       }
     }
-  }, [text, font]);
+  }, [text, font, fontColor, backgroundColor]);
 
   const handleDownload = () => {
-    const canvas = canvasRef.current;
-    if (canvas) {
+    const newCanvas = document.createElement('canvas');
+    newCanvas.width = 2893;
+    newCanvas.height = 4092;
+
+    if (newCanvas) {
+      const newCtx = newCanvas.getContext('2d');
+      if (newCtx) {
+        newCtx.clearRect(0, 0, newCanvas.width / 2, newCanvas.height / 2);
+
+        // Background color
+        newCtx.fillStyle = backgroundColor.hex;
+        newCtx.fillRect(0, 0, newCanvas.width, newCanvas.height);
+
+        newCtx.font = ['1600px', font].join(' ');
+        newCtx.textAlign = 'center';
+        newCtx.textBaseline = 'middle';
+
+        newCtx.fillStyle = fontColor.hex;
+        newCtx.fillText(text, newCanvas.width / 2, newCanvas.height / 2);
+      }
       const link = document.createElement('a');
       link.download = 'bib.png';
-      link.href = canvas.toDataURL();
+      link.href = newCanvas.toDataURL();
       link.click();
     }
   };
@@ -42,7 +92,12 @@ export const Generator = () => {
   return (
     <div>
       <div className="space-y-4">
-        <canvas ref={canvasRef} width={300} height={100} />
+        <canvas
+          className="forced-color-adjust-none"
+          ref={canvasRef}
+          width={300}
+          height={100}
+        />
 
         <Input
           value={text}
@@ -65,9 +120,28 @@ export const Generator = () => {
             <SelectItem value="Roboto">Roboto</SelectItem>
           </SelectContent>
         </Select>
+        <div className="flex justify-around">
+          <div className="flex items-center space-x-2">
+            <Label htmlFor="font-color-picker">文字色</Label>
+            <InputColor
+              key="font-color-picker"
+              initialValue="#252525"
+              onChange={setFontColor}
+            />
+          </div>
+
+          <div className="flex items-center space-x-2">
+            <Label htmlFor="background-color-picker">背景色</Label>
+            <InputColor
+              key="background-color-picker"
+              initialValue="#FFF"
+              onChange={setBackgroundColor}
+            />
+          </div>
+        </div>
       </div>
 
-      <Button className="mt-10" onClick={handleDownload}>
+      <Button className="mt-6" onClick={handleDownload}>
         ダウンロード
       </Button>
     </div>
--- src/lib/layout/Header.tsx
+++ src/lib/layout/Header.tsx
@@ -1,13 +1,11 @@
-// import { ThemeToggle } from '@/lib/components/theme-toggle';
+import { ThemeToggle } from '@/lib/components/theme-toggle';
 
 const Header = () => {
   return (
     <header className="bg-base-100/80 sticky top-0 z-10 w-full backdrop-blur-md">
       <section className="wrapper mx-auto flex items-center justify-between py-2">
         <div className="ml-auto">
-          {/*
-            <ThemeToggle />
-          */}
+          <ThemeToggle />
         </div>
       </section>
     </header>
